716f805815f2caf6b1ffc43c258989d36d22d533
ChengBang Zhou <zhouchengbang@gmail.com>
2018-10-08 16:45:29 +0800

    plot decision region

--- src/chapter2/iris.py
+++ src/chapter2/iris.py
@@ -14,7 +14,13 @@ def plot_decision_regions(X, y, classifier, resolution = 0.02):
     x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
     x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
     xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution), np.arange(x2_min, x2_max, resolution))
-    Z = classifier.predict(np.array([xx1.]))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+    for idx, c1 in enumerate(np.unique(y)):
+        plt.scatter(x=X[y==c1,0],y=X[y==c1,1],alpha=0.8,c=cmap(idx),marker=markers[idx],label=c1)
 
 
 
@@ -39,6 +45,13 @@ if __name__ == '__main__':
     plt.plot(range(1, len(ppn.errors_) + 1), ppn.errors_, marker = 'o')
     plt.xlabel('Epochs')
     plt.ylabel('Number of misclassification')
+
+    plt.figure(3)
+    plot_decision_regions(X, y, classifier=ppn)
+    plt.xlabel('sepal length')
+    plt.ylabel('petal length')
+    plt.legend(loc='upper left')
+
     plt.show()
 
 
